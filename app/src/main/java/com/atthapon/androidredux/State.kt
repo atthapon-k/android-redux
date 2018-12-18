b246b36ce1fd46f0b1ee8d4a77f536367dce16bf
Atthapon Korkaew <atthapon@onedaycat.com>
2018-12-18 21:38:13 +0700

    Add Example

---
+++ app/src/main/java/com/atthapon/androidredux/State.kt
@@ -0,0 +1,9 @@
+package com.atthapon.androidredux
+
+/**
+ * Created by Atthapon Korkaew on 18/12/2018 AD.
+ * Copyright © 2018 Onedaycat. All rights reserved.
+ */
+interface State {
+    val userState: UserState
+}
