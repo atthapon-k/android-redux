97675e8e645fa1e65fc14f5d9fd148db997915b0
atthapon-k <polcpekusrc@gmail.com>
2018-12-18 09:38:57 +0700

    Add AppState interface

--- androidreduxlib/src/main/java/com/atthapon/androidreduxlib/redroid/RxReDroid.kt
+++ androidreduxlib/src/main/java/com/atthapon/androidreduxlib/redroid/RxReDroid.kt
@@ -6,23 +6,23 @@ import io.reactivex.disposables.Disposable
 
 object RxReDroid {
     @JvmStatic
-    fun <State> flowable(store: ReDroidStore<State>): Flowable<ReDroidStore<State>> {
-        return Flowable.create<ReDroidStore<State>>({ emitter ->
-                                                        val subscription = store.subscribe {
-                                                            emitter.onNext(store)
-                                                        }
+    fun flowable(store: ReDroidStore): Flowable<ReDroidStore> {
+        return Flowable.create<ReDroidStore>({ emitter ->
+            val subscription = store.subscribe {
+                emitter.onNext(store)
+            }
 
-                                                        var isDisposed = false
-                                                        emitter.setDisposable(object: Disposable {
-                                                            override fun isDisposed(): Boolean = isDisposed
+            var isDisposed = false
+            emitter.setDisposable(object : Disposable {
+                override fun isDisposed(): Boolean = isDisposed
 
-                                                            override fun dispose() {
-                                                                if (isDisposed) return
-                                                                isDisposed = true
-                                                                subscription.invoke()
-                                                            }
+                override fun dispose() {
+                    if (isDisposed) return
+                    isDisposed = true
+                    subscription.invoke()
+                }
 
-                                                        })
-                                                    }, BackpressureStrategy.MISSING)
+            })
+        }, BackpressureStrategy.MISSING)
     }
 }
