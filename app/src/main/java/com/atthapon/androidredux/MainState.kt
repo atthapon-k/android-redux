b877f38f7f5a9e45db4b4e0126301e7bcfd9196b
Atthapon Korkaew <atthapon@onedaycat.com>
2018-12-20 22:24:27 +0700

    Refactor package and library name

--- app/src/main/java/com/atthapon/androidredux/MainState.kt
+++
@@ -1,9 +0,0 @@
-package com.atthapon.androidredux
-
-/**
- * Created by Atthapon Korkaew on 18/12/2018 AD.
- * Copyright © 2018 Onedaycat. All rights reserved.
- */
-class MainState : State {
-    override val userState: UserState = UserState()
-}
--- app/src/main/java/com/atthapon/androidredux/UserState.kt
+++
@@ -1,7 +0,0 @@
-package com.atthapon.androidredux
-
-/**
- * Created by Atthapon Korkaew on 18/12/2018 AD.
- * Copyright © 2018 Onedaycat. All rights reserved.
- */
-data class UserState(val users: ArrayList<User> = ArrayList())
